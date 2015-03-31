0a66cd9965c46631c1d6771a2dab6578fe1e7898
Tony DiNitto <think41c@gmail.com>
2015-03-30 23:31:22 -0600

    New method structure implemented.

--- phrase.rb
+++ phrase.rb
@@ -1,21 +1,19 @@
 class Phrase
 
   def initialize(phrase)
-
-    @word_count = {}
+    @word_count = Hash.new(0)
+    @phrase = phrase.split
+    @word_count = { 'word' => 1 }
   end
 
   def word_count
-    words_counted = { 'word' => 1 }
-    words_counted
+    @word_count
   end
 end
 
 ##########
 # Basic logic sandbox to use for this class #########
-# a = "cat cat dog"
-# a = a.split
-# p a
+# a = "cat cat dog".split
 # b = Hash.new(0)
 # a.each do |x|
 #   b[x] += 1
