df9fde6850ea84c59c1780fb5796c90f014a2a63
Tony DiNitto <think41c@gmail.com>
2015-03-31 13:26:59 -0600

    Able to handle mixed cased words. Need to use regex or gsub for other word lists.

--- phrase.rb
+++ phrase.rb
@@ -4,18 +4,10 @@ class Phrase
 
   def initialize(phrase)
     @word_count = Hash.new(0)
-    phrase = phrase.split
+    phrase = phrase.downcase.split   # Ignores case, splits on 'space' 
+    # phrase = phrase.split(',')       # Splits again, splits on ',' 
     phrase.each { |word| @word_count[word] += 1 }
     @word_count 
   end
 
 end
-
-##########
-# Basic logic sandbox to use for this class #########
-# a = "cat cat dog".split
-# b = Hash.new(0)
-# a.each do |x|
-#   b[x] += 1
-# end
-####################
