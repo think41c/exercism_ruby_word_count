9c84dcb1df89870d95fafa5039661de412a25608
Tony DiNitto <think41c@gmail.com>
2015-04-01 23:47:27 -0600

    Refactor phrase parsing operations.

--- phrase.rb
+++ phrase.rb
@@ -4,8 +4,7 @@ class Phrase
 
   def initialize(phrase)
     @word_count = Hash.new(0)
-    phrase = phrase.gsub(/[!@#$%^&*()-:;]/, '')
-    phrase = phrase.downcase.split   # Ignores case, splits on 'space' 
+    phrase = phrase.gsub(/[!@#$%^&*()-:;]/, '').downcase.split
     # phrase = phrase.split(',')       # Splits again, splits on ',' 
     phrase.each { |word| @word_count[word] += 1 }
     @word_count 
