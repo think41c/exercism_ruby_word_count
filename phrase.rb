cf9644132a52e75cf81fe1500d3a720575c2bf19
Tony DiNitto <think41c@gmail.com>
2015-04-03 23:44:37 -0600

    Refactored to use reduce enum.

--- phrase.rb
+++ phrase.rb
@@ -3,10 +3,13 @@ class Phrase
   attr_reader :word_count
 
   def initialize(phrase)
-    @word_count = Hash.new(0)
     phrase = phrase.gsub("," , " ")
     phrase = phrase.gsub(/[!@#$%^&.*:]/, "").downcase.split
-    phrase.each { |word| @word_count[word] += 1 }
-  end
+    
+    @word_count = phrase.reduce(Hash.new(0)) do |memo, word| 
+      memo[word] += 1
+      memo
+    end
 
+  end
 end
