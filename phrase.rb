fe39474f704ab9cb73e1b374d9bfd051fa6fb2b4
Tony DiNitto <think41c@gmail.com>
2015-04-11 23:45:25 -0600

    Refactored to extract responsibility of methods.

--- phrase.rb
+++ phrase.rb
@@ -3,11 +3,14 @@ class Phrase
   attr_reader :word_count
 
   def initialize(phrase)
-    phrase = phrase.gsub(/[!@#$%^&*():.,]/, " ").downcase.split
+    @phrase = phrase.gsub(/[!@#$%^&*():.,]/, " ").downcase.split
+    count_words
+  end
 
-    @word_count = phrase.reduce(Hash.new(0)) do |memo, word|
+  def count_words
+    @word_count = @phrase.reduce(Hash.new(0)) do |memo, word|
       memo[word] += 1 
       memo
-    end
+    end  
   end
 end
