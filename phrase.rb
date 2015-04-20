4b8200d40e44747f7929f8b0e33e1383b3fd6506
Tony DiNitto <think41c@gmail.com>
2015-04-20 19:31:50 -0400

    Removed unncessary instance var.

--- phrase.rb
+++ phrase.rb
@@ -3,10 +3,10 @@ class Phrase
   attr_reader :word_count
 
   def initialize(phrase)
-    @phrase = phrase.gsub(/[^\w']/, " ").downcase.split
-    @word_count = @phrase.each_with_object(Hash.new(0)) do |word, memo|
+    phrase = phrase.gsub(/[^\w']/, " ").downcase.split
+    @word_count = phrase.each_with_object(Hash.new(0)) do |word, memo|
       memo[word] += 1
     end
-  
   end
+
 end
