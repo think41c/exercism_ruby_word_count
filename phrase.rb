78dfd7244af95735f0d547cb899d8d9ef6a68bca
Tony DiNitto <think41c@gmail.com>
2015-04-23 00:00:39 -0400

    Refactored word count method.

--- phrase.rb
+++ phrase.rb
@@ -3,10 +3,17 @@ class Phrase
   attr_reader :word_count
 
   def initialize(phrase)
-    phrase = phrase.gsub(/[^\w']/, " ").downcase.split
-    @word_count = phrase.each_with_object(Hash.new(0)) do |word, memo|
-      memo[word] += 1
-    end
+    @phrase = phrase.gsub(/[^\w']/, " ").downcase.split
+    word_count
+    # @word_count = phrase.each_with_object(Hash.new(0)) do |word, memo|
+      # memo[word] += 1
+    # end
   end
 
+  def word_count
+    @phrase.each_with_object(Hash.new(0)) do |word, memo|
+    memo[word] += 1
+    end
+  end
 end
+
