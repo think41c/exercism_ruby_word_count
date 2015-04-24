0b6eb763886488e283d6199f316cbe0289c73204
Tony DiNitto <think41c@gmail.com>
2015-04-23 21:00:24 -0400

    Refactor - put a parsing method in as a private method.

--- phrase.rb
+++ phrase.rb
@@ -1,12 +1,19 @@
 class Phrase
 
   def initialize(phrase)
-    @phrase = phrase.gsub(/[^\w']/, " ").downcase.split
+    @phrase = phrase
+    parse_input
   end
 
   def word_count
     @phrase.each_with_object(Hash.new(0)) do |word, memo|
       memo[word] += 1
     end
+  end
+
+  private 
+
+  def parse_input
+    @phrase = @phrase.gsub(/[^\w']/, " ").downcase.split    
   end
 end
